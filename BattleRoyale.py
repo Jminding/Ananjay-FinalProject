b067025756fbb393220d1b57d0cc06cbc9e6844a
Jminding <jtding43@gmail.com>
2022-08-31 17:31:23 -0400

    Battle Royale

--- BattleRoyale.py
+++ BattleRoyale.py
@@ -2,7 +2,7 @@ import random
 import time
 
 contestants = ["Ananjay", "Noah", "Daniel", "Landon", "Sean", "Olivia", "Edward", "Jay", "Donald Trump", "Joe Biden", "Hillary Clinton", "Kim Jong Un", "Greta Thunberg", "Vladimir Putin"]
-deaths = [""]
+deaths = ["committed suicide", "ate poison berries", "got eaten by wolves", "was caught playing among us and died", "got hit on the head"]
 print(f"Contestants: {contestants}")
 while len(contestants) > 1:
     print(f"Remaining contestants: {len(contestants)}")
@@ -10,7 +10,7 @@ while len(contestants) > 1:
     one = round_contestants[0]
     two = round_contestants[1]
     print(f"{one} vs. {two}!")
-    time.sleep(2)
+    time.sleep(3)
     winner = random.choice([one, two])
     loser = None
     if winner == one:
@@ -19,5 +19,13 @@ while len(contestants) > 1:
         loser = one
     print(f"{winner} wins!")
     contestants.remove(loser)
+    print()
+    die = random.randint(1, 100)
+    if die < 50:
+        continue
+    else:
+        death = random.choice(contestants)
+        print(f"{death} {random.choice(deaths)}")
+        contestants.remove(death)
     print()
 print(f"Winner is: {contestants[0]}!")
